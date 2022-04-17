8d79dbf1050f389c875ff7566afea0b8f1dbfe14
Gui <gsetienne@free.fr>
2022-04-17 11:30:03 +0200

    burger en js

---
+++ js/burger.js
@@ -0,0 +1,18 @@
+console.log("JS Lancé mec !")
+var sidenav = document.getElementById("mySidenav");
+var openBtn = document.getElementById("openBtn");
+// var openBtn = document.getElementsByClassName("burger-icon")
+var closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  sidenav.classList.add("active");
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  sidenav.classList.remove("active");
+}
