f7acee9b0a3657683147f61826f0f92ddd210209
Gui <gsetienne@free.fr>
2022-06-23 17:48:55 +0200

    es 1

--- js/swicthlang.js
+++ js/swicthlang.js
@@ -1,10 +1,10 @@
 // detect lang in localSto and apply.
 switch(localStorage.getItem("lang")){
     case 'en':
-        window.location.href = "../en/index.html";
+        window.location.href = "./en/index.html";
         break
     case 'es':
-        window.location.href = "../es/index.html";
+        window.location.href = "./es/index.html";
         break
     case 'fr':
         console.log("on a coché FR fils !")
